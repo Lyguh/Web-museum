5707fed487a26df24a482c432799c974d6950d96
Daniil <127053306+Lyguh@users.noreply.github.com>
2025-12-22 15:50:18 +0300

    Make adaptiving baner

--- museum/src/app/(pages)/page.tsx
+++ museum/src/app/(pages)/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import Container from "@/components/shared/container";
+import HeroBaner from "@/components/section/hero-baner";
 
 export default function Home() {
   return (
     <div className="">
       <main>
+          <Container>
+              <HeroBaner />
+          </Container>
 
       </main>
     </div>
