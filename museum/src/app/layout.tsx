45a605fa8df7e9383a9a0e77d92474ade25a26af
Daniil <127053306+Lyguh@users.noreply.github.com>
2025-12-22 20:44:30 +0300

    make heroPage

--- museum/src/app/layout.tsx
+++ museum/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/blocks/header";
+import Footer from "@/components/blocks/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-[#FFF3E6]`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
